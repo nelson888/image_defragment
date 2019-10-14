b65b341114b27345526fd86839ae7a1e832cf663
nelson888 <tambue@hotmail.fr>
2019-10-14 23:26:30 +0200

    added comments

--- main.cpp
+++ main.cpp
@@ -5,34 +5,12 @@
 #include <sstream>
 #include <string>
 
-/** objectifs: 2 binaires:
- * 1) binaire qui calcule a partir des fragments l'image reconstruit
- * 2) binaire qui a le fichier de position des fragments attendu (fichier du prof)
- * et un autre fichier qui correspond aux position des fragments qu'on a eu
-
-**/
-
 using namespace std;
 using namespace cv;
 
 const int LOADING_TYPE = CV_LOAD_IMAGE_GRAYSCALE;
-const int AUGMENTED_COLS = 500;
-const int AUGMENTED_ROWS = 500;
-/*
-void rotate(Mat* fragment, Point2f center, float angle) {
-  Mat rot = cv::getRotationMatrix2D(center, angle, 1.0);
-  // determine bounding rectangle, center not relevant
-  Rect2f bbox = cv::RotatedRect(Point2f(), fragment->size(), angle).boundingRect2f();
-  // adjust transformation matrix
-  rot.at<double>(0,2) += bbox.width/2.0 - fragment->cols/2.0;
-  rot.at<double>(1,2) += bbox.height/2.0 - fragment->rows/2.0;
+const int BIG_AUGMENTATION = 500;
 
-  Mat dst;
-  warpAffine((*fragment), dst, rot, bbox.size());
-  (*fragment) = dst;  
-
-}
-*/
 
 Mat rotatedFragment(Mat* fragment, Point2f center, float angle) {
   Mat rotationMatrix = getRotationMatrix2D(center, angle, 1.0);
@@ -42,11 +20,14 @@ Mat rotatedFragment(Mat* fragment, Point2f center, float angle) {
 }
 
 void putFragment(Mat* imageOut, Mat* fragment, int x, int y, float angle) {
-  Rect area(x + AUGMENTED_COLS / 2.0f, y + AUGMENTED_ROWS / 2.0f, fragment->cols, fragment->rows);
+  Rect area(x + BIG_AUGMENTATION / 2.0f, y + BIG_AUGMENTATION / 2.0f, fragment->cols, fragment->rows);
   Point2f rotationCenter(fragment->cols / 2.0f, fragment->rows / 2.0f);
   rotatedFragment(fragment, rotationCenter, angle).copyTo((*imageOut)(area));
 }
 
+/**
+* Image should be passed as an argument
+**/
 int main(int argc, char** argv){
 	Mat imageIn = imread( argv[1], LOADING_TYPE );
 	if(!imageIn.data) {
@@ -55,7 +36,7 @@ int main(int argc, char** argv){
   }
 
 	// create empty image bigger than the original (to avoid problems when writting rotated images)
-	Mat bigImageOut(imageIn.rows + AUGMENTED_ROWS, imageIn.cols + AUGMENTED_COLS, 0);
+	Mat bigImageOut(imageIn.rows + BIG_AUGMENTATION, imageIn.cols + BIG_AUGMENTATION, 0);
 
   // reads fragments.txt
 	ifstream infile("fragments.txt");
@@ -73,15 +54,11 @@ int main(int argc, char** argv){
 	}
 
   // Crop the big image to have the correct size (Region Of Interest)
-  Rect regionOfInterest(AUGMENTED_COLS / 2.0f, AUGMENTED_ROWS / 2.0f, imageIn.cols, imageIn.rows);
+  Rect regionOfInterest(BIG_AUGMENTATION / 2.0f, BIG_AUGMENTATION / 2.0f, imageIn.cols, imageIn.rows);
   Mat imageOut = bigImageOut(regionOfInterest);
-	//imshow( "Display window", imageOut);   
+	imshow( "Display window", imageOut);   
 	imwrite("./result.png", imageOut);          
 	waitKey(0);       	
-	
-	std::cout << "IN : " << imageIn.rows << ' ' << imageIn.cols << endl;
-	std::cout << "BIG: " << bigImageOut.rows << ' ' << bigImageOut.cols << endl;
-	std::cout << "RES: " << imageOut.rows << ' ' << imageOut.cols << endl;
 	return 0;
 }
 
