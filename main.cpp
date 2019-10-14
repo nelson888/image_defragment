c58cb3068ddb0ba227185f67e12559dad0d69b1a
nelson888 <tambue@hotmail.fr>
2019-10-14 14:59:17 +0200

    changed main logic

--- main.cpp
+++ main.cpp
@@ -13,26 +13,12 @@
 using namespace std;
 using namespace cv;
 
-double thresh = 100;
 
-void processFragment(Mat imageIn, Mat imageOut, Mat fragment) {
-		//TODO
-		// finding contours
-		Mat canny_output;
-		vector<vector<Point> > contours;
-		vector<Vec4i> hierarchy;
-		
-		// Detect edges using canny
-		Canny( imageIn, canny_output, thresh, thresh*2, 3 );
-		// Find contours
-		findContours( canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
+void putFragment(Mat* imageOut, Mat* fragment, int x, int y, float angle) {
 
-		std::cout << contours.size() << endl;
 }
 
-
 int main(int argc, char** argv){
-
 
 
 	Mat imageIn = imread( argv[1], CV_LOAD_IMAGE_GRAYSCALE );
@@ -46,13 +32,11 @@ int main(int argc, char** argv){
    	}
 	
 	Mat imageOut(imageIn.rows, imageIn.cols, 0);
-       
-    for (int i=0; i < nbFragments; i++) {
-		std::string fileName = "./frag_eroded/frag_eroded_" + std::to_string(i) + ".png";
-		Mat fragmentImg = imread(fileName , CV_LOAD_IMAGE_GRAYSCALE );
-		std::cout << "Processing " << fileName << std::endl;
-		processFragment(imageIn, imageOut, fragmentImg);
-	}
+  
+  // TODO read the fragments.txt and for each line, extract the id, posX, posY, angle
+  //                                                then read the image 'frag_eroded/frag_eroded_{id}.png'
+  //                                                then call putFragment putFragment(&imageIn, &imageOut, posX, posY, angle)
+ 
 	
 	imshow( "Display window", imageOut );             
 	waitKey(0);       	
