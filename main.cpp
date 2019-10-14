38f5b043db9d5833eb999edba983de679ba784de
nelson888 <tambue@hotmail.fr>
2019-10-14 16:15:09 +0200

    started implement put fragment

--- main.cpp
+++ main.cpp
@@ -15,44 +15,42 @@
 using namespace std;
 using namespace cv;
 
+const int LOADING_TYPE = CV_LOAD_IMAGE_GRAYSCALE;
 
 void putFragment(Mat* imageOut, Mat* fragment, int x, int y, float angle) {
-
+ 
+  // TODO rotate fragment
+  Rect area(x, y, fragment->cols, fragment->rows);
+  fragment->copyTo((*imageOut)(area));
 }
 
 int main(int argc, char** argv){
+	Mat imageIn = imread( argv[1], LOADING_TYPE );
+	if(!imageIn.data) {
+    cout <<  "Could not open or find the image" << std::endl;
+    return -1;
+  }
 
-
-	Mat imageIn = imread( argv[1], CV_LOAD_IMAGE_GRAYSCALE );
-	
-	int nbFragments = 1; // TODO parse from command line argument
-	
-	if(! imageIn.data )                              // Check for invalid input
-    	{
-        	cout <<  "Could not open or find the image" << std::endl ;
-        	return -1;
-   	}
-	
+	// create empty image
 	Mat imageOut(imageIn.rows, imageIn.cols, 0);
   
-  // TODO read the fragments.txt and for each line, extract the id, posX, posY, angle
-  //                                                then read the image 'frag_eroded/frag_eroded_{id}.png'
-  //                                                then call putFragment putFragment(&imageIn, &imageOut, posX, posY, angle)
-  
+  // reads fragments.txt
 	ifstream infile("fragments.txt");
 	string line;
-	while (getline(infile, line){
+	while (getline(infile, line)) {
 		istringstream iss(line);
 		int id, posX, posY;
 		float angle;
-		if( !(iss >> id >> posX >> posY >> angle)){break;}
-		Mat fragment = imread("frag_eroded/frag_eroded_"+id+".png");
-		putFragment(&fragment, &imageOut, posX, posY, angle);
+		if(!(iss >> id >> posX >> posY >> angle)) {
+		  std::cout << "Error while parsing line: '" << line << "'" << endl;
+		  continue; 
+		}
+		Mat fragment = imread("./frag_eroded/frag_eroded_"+ to_string(id) +".png", LOADING_TYPE);
+		putFragment(&imageOut, &fragment, posX, posY, angle);
 	}
 	
 	imshow( "Display window", imageOut );             
 	waitKey(0);       	
-
 	
 	return 0;
 }
