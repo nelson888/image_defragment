7ee6abd453761d0118cdd20b859bd527e48822ca
headhunteerr <nicolas.mandadjiev@gmail.com>
2019-11-15 16:30:15 +0100

    - Added images - Now has parameters for Gaussian Blurring Syntax is : ./detectionCercle [image] [kernel size rows] [kernel size colums] [sigmaX]

--- part2/main.cpp
+++ part2/main.cpp
@@ -6,12 +6,20 @@ using namespace cv;
 
 int main(int argc, char const *argv[])
 {
-    if (argc != 1) {
+    if (argc == 5) {
         string imageName(argv[1]);
         Mat imageIN = imread("./images/"+imageName, CV_LOAD_IMAGE_UNCHANGED);
-        Mat imageProcessing;
-        GaussianBlur(imageIN, imageProcessing, Size(5,5), 25, 0);
+        Mat imageProcessing(imageIN.rows, imageIN.cols, CV_8UC4);
+        int g_kSize_Rows = stoi(argv[2]);
+        int g_kSize_Cols = stoi(argv[3]);
+        Size kernelSize(g_kSize_Rows, g_kSize_Cols);
+        double g_sigmaX = stod(argv[4]);
+        GaussianBlur(imageIN, imageProcessing, kernelSize, g_sigmaX, 0);
         imshow("My blurred image",imageProcessing);
+        waitKey(0);
+    }
+    else{
+        cout << "Syntax is : ./detectionCercle [image] [kernel size rows] [kernel size colums] [sigmaX]" << endl;
     }
     return 0;
 }
