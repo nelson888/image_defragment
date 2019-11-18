55c8c6543749ad4ee2f0aae3580e631a8aeb1df2
headhunteerr <nicolas.mandadjiev@gmail.com>
2019-11-18 11:44:21 +0100

    Added functions to prompt ints, doubles, strings Added function to get the parameters for the Gaussian blur

--- part2/main.cpp
+++ part2/main.cpp
@@ -1,25 +1,85 @@
 #include "opencv2/opencv.hpp"
+#include <iostream>
 #include <string>
 
 using namespace std;
 using namespace cv;
 
+class CircleDetection
+{
+private:
+    
+public:
+    
+    CircleDetection(/* args */);
+    ~CircleDetection();
+};
+
+CircleDetection::CircleDetection(/* args */)
+{
+}
+
+CircleDetection::~CircleDetection()
+{
+}
+
+double getDoubleInput(){
+    string s_double;
+    cin >> s_double;
+    return stod(s_double);
+}
+
+int getIntInput(){
+    int i;
+    cin >> i;
+    return i;
+}
+
+string getStringInput(){
+    string str; 
+    cin >> str;
+    return str;
+}
+
+void getGaussianBlurParameters(Size* kSize, double* g_SigmaX){
+    cout << "Récupération des paramètres pour le filtrage Gaussien." << endl;
+    cout << "Entrez le nombre de ligne du noyau : ";
+    kSize->height = getIntInput();
+    cout << "Entrez le nombre de colonne du noyau : ";
+    kSize->width = getIntInput();
+    cout << "Entrez la déviation standard en X : ";
+    *g_SigmaX = getDoubleInput();
+}
+
+void getSobelParameters()
+
 int main(int argc, char const *argv[])
 {
-    if (argc == 5) {
+    if (argc == 3) {
         string imageName(argv[1]);
         Mat imageIN = imread("./images/"+imageName, CV_LOAD_IMAGE_UNCHANGED);
         Mat imageProcessing(imageIN.rows, imageIN.cols, CV_8UC4);
-        int g_kSize_Rows = stoi(argv[2]);
-        int g_kSize_Cols = stoi(argv[3]);
-        Size kernelSize(g_kSize_Rows, g_kSize_Cols);
-        double g_sigmaX = stod(argv[4]);
-        GaussianBlur(imageIN, imageProcessing, kernelSize, g_sigmaX, 0);
+        if (*argv[2] == 'y'){
+            Size kernelSize(0,0);
+            double g_sigmaX;
+            getGaussianBlurParameters(&kernelSize, &g_sigmaX);
+            GaussianBlur(imageIN, imageProcessing, kernelSize, g_sigmaX);
+        }        
         imshow("My blurred image",imageProcessing);
         waitKey(0);
     }
     else{
-        cout << "Syntax is : ./detectionCercle [image] [kernel size rows] [kernel size colums] [sigmaX]" << endl;
+        cout << "Syntax is : ./detectionCercle [image] [gaussian blur: y/n]" << endl;
     }
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
