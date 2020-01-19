25b2e806ee607b36b60a938a3bb1dd615e56ff41
nelson888 <tambue@hotmail.fr>
2020-01-19 17:50:54 +0100

    added part 3

---
+++ tp3/main.cpp
@@ -0,0 +1,18 @@
+#include <opencv2/opencv.hpp>
+#include <stdio.h>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+using namespace cv;
+
+
+int main(int argc, char** argv){
+
+
+	
+	return 0;
+}
+
