d5cd6c18d02479b99aa9fb5627b1c9fea42bcfcf
nelson888 <tambue@hotmail.fr>
2020-01-26 23:15:58 +0100

    implemented ransac

--- tp3/main.cpp
+++ tp3/main.cpp
@@ -1,9 +1,91 @@
 #include <iostream>
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
 #include "opencv2/opencv.hpp"
 
 using namespace cv;
 
+struct AngledPosition {
+    bool none; // true if no result was found
+    int x;
+    int y;
+    float angle;
+};
+
 const int LOADING_TYPE = CV_LOAD_IMAGE_UNCHANGED;
+const int MAX_TRIALS = 8;
+
+const int MIN_POINTS = 2;
+const int D = 3; // Number of close data points required to assert that a model fits well to data.
+
+bool vecContains(std::vector<DMatch>* matches, DMatch dMatch) {
+    for (auto match: *matches) {
+        if (abs(dMatch.distance - match.distance) < 0.2f &&
+            dMatch.queryIdx == match.queryIdx &&  dMatch.trainIdx == match.trainIdx) {
+            return true;
+        }
+    }
+    return false;
+}
+
+std::vector<DMatch> getNRandomValues(int n, std::vector<DMatch>* matches) {
+    std::vector<DMatch> result;
+    if (matches->size() <= n) {
+        for (auto match: *matches) {
+            result.push_back(match);
+        }
+    } else {
+        while (result.size() < n) {
+            DMatch m = (*matches)[rand() % n];
+            if (!vecContains(&result, m)) {
+                result.push_back(m);
+            }
+        }
+    }
+    return result;
+}
+
+bool matchesLargerThanImage(std::vector< DMatch>* matches, std::vector<KeyPoint>* keypoints_image, std::vector<KeyPoint>* keypoints_fragment) {
+    for (int i = 0; i < matches->size() / 2; ++i) {
+        for (int j = 0; j < matches->size(); ++j) {
+            if (i == j) {
+                continue;
+            }
+            if (abs((*keypoints_fragment)[(*matches)[i].queryIdx].pt.x - (*keypoints_image)[(*matches)[j].queryIdx].pt.x) <  20 &&
+                abs((*keypoints_fragment)[(*matches)[i].queryIdx].pt.y - (*keypoints_image)[(*matches)[j].queryIdx].pt.y) < 20 ) {
+                return true;
+            }
+        }
+    }
+}
+
+// pas reussi a implementer ransac
+AngledPosition ransac(std::vector<KeyPoint>* keypoints_image,
+                      std::vector<KeyPoint>* keypoints_fragment,
+                      std::vector< DMatch>* matches) {
+    int n = matches->size();
+    AngledPosition result;
+    result.none = true;
+
+    for (int i = 0; i < MAX_TRIALS; ++i) {
+        int k=0;
+        AngledPosition temp;
+        temp.x = 0; temp.y = 0; temp.angle = 0; temp.none = false;
+        std::vector<DMatch> values = getNRandomValues(MIN_POINTS, matches);
+        if (matchesLargerThanImage(&values, keypoints_image, keypoints_fragment)) {
+            continue;
+        }
+        for (auto match : values) {
+            temp.x += (*keypoints_fragment)[match.queryIdx].pt.x;
+            temp.y += (*keypoints_fragment)[match.queryIdx].pt.y;
+            temp.angle += (*keypoints_fragment)[match.queryIdx].angle;
+        }
+        temp.x /= values.size(); temp.y /= values.size(); temp.angle /= ((float)values.size());
+        result = temp;
+
+    }
+    return result;
+}
 
 void getKeyPoints(Mat image, Mat fragment, std::vector<KeyPoint>* keypoints_image,
         std::vector<KeyPoint>* keypoints_fragment,
@@ -75,6 +157,12 @@ int main(int argc, char** argv)
 
     getKeyPoints(image, fragment, &keypoints_image, &keypoints_fragment,
                  &good_matches);
+    AngledPosition position = ransac(&keypoints_image, &keypoints_fragment, &good_matches);
+    if (position.none) {
+        std::cout << "No best point was found" << std::endl;
+    } else {
+        std::cout << "Found best point (" << position.x << ", " << position.y << ") with angle " << position.angle << std::endl;
+    }
 
     Mat img_matches;
 
@@ -115,9 +203,9 @@ int main(int argc, char** argv)
     line(img_matches, scene_corners[3] + Point2f(fragment.cols, 0), scene_corners[0] + Point2f(fragment.cols, 0), Scalar(0, 255, 0), 4);
 
     //-- Show detected matches
-    imshow("Good Matches & Object detection", img_matches);
+   // imshow("Good Matches & Object detection", img_matches);
     imwrite( "./result.jpg", img_matches );
+//    waitKey(0);
 
-    waitKey(0);
     return 0;
 }
